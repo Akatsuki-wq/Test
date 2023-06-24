9a044b19a24dd9e7132dcf5e3c060b625413cfd4
DESKTOP-RKBA8CC\Administrator <Akatsuki-J@outlook.com>
2023-06-24 22:58:20 +0800

    测试加载lua文件

--- Assets/Scripts/TestLua.cs
+++ Assets/Scripts/TestLua.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using XLua;
 
 public class TestLua : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        LuaEnv lua = new LuaEnv();
+        lua.AddLoader((ref string filename) =>
+        {
+            if (filename == "LuaTest")
+            {
+                //string script = Application.dataPath + "/LuaScripts/" + filename + ".lua";
+                string script = "G:/gongcheng/" + filename + ".lua";
+                string strLuaContent = File.ReadAllText(script);
 
+                return System.Text.Encoding.UTF8.GetBytes(strLuaContent);
+            }
+            return null;
+        });
+        lua.DoString("require 'LuaTest'");
     }
 
     // Update is called once per frame
